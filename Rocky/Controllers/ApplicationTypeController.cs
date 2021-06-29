e492fdff974773bcf32a035fc034c4c08bf67515
ttomczak3 <ttomczak3@student.cvtc.edu>
2021-06-28 21:17:56 -0500

    Edit Application Type

--- Rocky/Controllers/ApplicationTypeController.cs
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -38,5 +38,35 @@ namespace Rocky.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        // GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // POST - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
